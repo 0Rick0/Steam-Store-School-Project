b6c474528a085992eb8cc82ae238e6b62230aea2
Rick Rongen <r.rongen@student.fontys.nl>
2015-06-14 22:13:42 +0200

    Added ability to search for people

--- SteamStore/SteamStore/search.aspx.cs
+++ SteamStore/SteamStore/search.aspx.cs
@@ -23,37 +23,69 @@ namespace SteamStore
             //check if the variables are there
             if (string.IsNullOrEmpty(Request.QueryString["q"]))
             {
-                Response.Redirect("error.aspx?errorMessage="+Server.UrlEncode("No search query!"));
+                Response.Redirect("error.aspx?errorMessage=" + Server.UrlEncode("No search query!"));
             }
 
             //make regex readable query
-// ReSharper disable once PossibleNullReferenceException already checked above
-            var regexQuery = "(" + Server.UrlDecode(Request.QueryString["q"]).Replace(" ",")|(") + ")";
+            // ReSharper disable once PossibleNullReferenceException already checked above
+            var regexQuery = "(" + Server.UrlDecode(Request.QueryString["q"]).Replace(" ", ")|(") + ")";
 
             //ask database
             using (var con = DbProvider.GetOracleConnection())
             {
-                var com = con.CreateCommand();
-                com.CommandText = "SELECT a.appid, appname " +
-                                  "FROM app a, (SELECT appid, listagg(ai.title ||' ' || ai.text) WITHIN GROUP(ORDER BY ai.appid) as descconc " +
-                                  "FROM APPSTOREITEM ai " +
-                                  "GROUP BY appid) dc " +
-                                  "WHERE a.appid = dc.appid " +
-                                  "AND REGEXP_SUBSTR(a.appid || categorieid || appname || dc.descconc,:qer,1,1,'i') IS NOT NULL";
-                var pQ = com.CreateParameter();
-                pQ.Direction = ParameterDirection.Input;
-                pQ.DbType = DbType.String;
-                pQ.ParameterName = "qer";
-                pQ.Value = regexQuery;
-                com.Parameters.Add(pQ);
-
-                var r = com.ExecuteReader();
-                while (r.Read())
+                using (var com = con.CreateCommand())
                 {
-                    //fill view
-                    var lc = new LiteralControl();
-                    lc.Text = string.Format("<div class=\"searchResult\"><a href=\"/game.aspx?appId={0}\">{1}</a></div>",r["appid"],r["appname"]);
-                    innerContent.Controls.Add(lc);
+                    com.CommandText = "SELECT a.appid, appname " +
+                                      "FROM app a, (SELECT appid, listagg(ai.title ||' ' || ai.text) WITHIN GROUP(ORDER BY ai.appid) as descconc " +
+                                      "FROM APPSTOREITEM ai " +
+                                      "GROUP BY appid) dc " +
+                                      "WHERE a.appid = dc.appid " +
+                                      "AND REGEXP_SUBSTR(a.appid || categorieid || appname || dc.descconc,:qer,1,1,'i') IS NOT NULL";
+                    var pQ = com.CreateParameter();
+                    pQ.Direction = ParameterDirection.Input;
+                    pQ.DbType = DbType.String;
+                    pQ.ParameterName = "qer";
+                    pQ.Value = regexQuery;
+                    com.Parameters.Add(pQ);
+
+                    var r = com.ExecuteReader();
+                    while (r.Read())
+                    {
+                        //fill view
+                        var lc = new LiteralControl();
+                        lc.Text = string.Format("<div class=\"searchResult\"><a href=\"/game.aspx?appId={0}\">{1}</a></div>", r["appid"], r["appname"]);
+                        innerContent.Controls.Add(lc);
+                    }
+                }
+
+                using (var com = con.CreateCommand())
+                {
+                    com.CommandText = "SELECT userid, username " +
+                                      "FROM steam_user " +
+                                      "WHERE REGEXP_SUBSTR(username||userid,:qer,1,1,'i') IS NOT NULL";
+                    var pQ = com.CreateParameter();
+                    pQ.Direction = ParameterDirection.Input;
+                    pQ.DbType = DbType.String;
+                    pQ.ParameterName = "qer";
+                    pQ.Value = regexQuery;
+                    com.Parameters.Add(pQ);
+
+                    var r = com.ExecuteReader();
+
+                    if (r.HasRows)
+                    {
+                        var spacer = new LiteralControl();
+                        spacer.Text = string.Format("<h2>Users</h2>");
+                        innerContent.Controls.Add(spacer);
+                    }
+
+                    while (r.Read())
+                    {
+                        //fill view
+                        var lc = new LiteralControl();
+                        lc.Text = string.Format("<div class=\"searchResult\"><a href=\"/profile.aspx?userId={0}\">{1}</a></div>", r["userid"], r["username"]);
+                        innerContent.Controls.Add(lc);
+                    }
                 }
             }
         }
